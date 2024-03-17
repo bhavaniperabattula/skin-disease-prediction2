945b150580b42b0b7d26f8bfbad7f202a33c0adc
PERABATTULA BHAVANI <78523131+bhavaniperabattula@users.noreply.github.com>
2024-03-17 09:44:18 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,31 +1,33 @@
-from flask import Flask,request, url_for, redirect, render_template
-import pickle
+from flask import Flask, render_template, request
+import tensorflow as tf
+from PIL import Image
 import numpy as np
 
 app = Flask(__name__)
 
-model=pickle.load(open('model.pkl','rb'))
+# Load your pre-trained deep learning model
+model = tf.keras.models.load_model('your_model_path')
 
+# Define class names for diseases (example)
+class_names = ['Disease A', 'Disease B', 'Disease C', 'Disease D', 'Disease E']
 
 @app.route('/')
-def hello_world():
-    return render_template("forest_fire.html")
+def index():
+    return render_template('index.html')
 
-
-@app.route('/predict',methods=['POST','GET'])
-def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=model.predict_proba(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
-
-    if output>str(0.5):
-        return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
-    else:
-        return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
-
+@app.route('/upload', methods=['POST'])
+def upload():
+    image_file = request.files['image']
+    if image_file:
+        img = Image.open(image_file)
+        img = img.resize((224, 224))  # Resize image according to your model input size
+        img_array = np.array(img) / 255.0  # Normalize image
+        img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+        prediction = model.predict(img_array)
+        predicted_class_index = np.argmax(prediction)
+        disease_name = class_names[predicted_class_index]
+        return disease_name
+    return 'Error: No image provided.'
 
 if __name__ == '__main__':
     app.run(debug=True)
