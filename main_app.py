643b97e9acdc7067061d0f807531ce592e9cfc0f
PERABATTULA BHAVANI <78523131+bhavaniperabattula@users.noreply.github.com>
2024-03-17 10:37:59 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -9,7 +9,13 @@ app = Flask(__name__)
 model = tf.keras.models.load_model('your_model_path')
 
 # Define class names for diseases (example)
-class_names = ['Disease A', 'Disease B', 'Disease C', 'Disease D', 'Disease E']
+class_names = [4: ('nv', 'Melanocytic nevi', ['Sensitivity to touch around the mole', 'Redness or inflammation around the mole'],['Avoid tight Clothing','Limit exposure to direct sunlight']),
+    6: ('mel', 'Melanoma', ['Multiple colors within a mole', 'Bleeding or oozing from a mole'],['Eat a balanced diet rich in antioxidants and vitamins','Avoid smoking and limit alcohol consumption']),
+    2: ('bkl', 'Benign keratosis-like lesions',  ['Itching or irritation in affected areas, Round or oval shaped growths', 'Very small growths clustered around the eyes or elsewhere on the face'],['Moisturize Regularly','Manage Stress by meditation or yoga']),
+    1: ('bcc', 'Basal cell carcinoma', ['Surrounding skin becoming sunken or depressed','Formation of a flesh-coloured, pearl like bump'],['Avoid harmful chemicals','Wear Protective Clothing']),
+    5: ('vasc', 'Pyogenic granulomas and hemorrhage', ['Prone to Ulceration', 'Moist or friable surface structure'],['Use sunscreen with a high SPF','Keep the affected arear covered with a sterile dressing']),
+    0: ('akiec', 'Actinic keratoses and intraepithelial carcinomae', ['Swelling and burning in affected region', 'Thickening of the skin'],['Avoid tanning beds and sunlamps','Avoid hot shower and opt for lukewarm water']),
+    3: ('df', 'Dermatofibroma', ['Dimpled appearance when pressed', 'Growing in size over time'],['Avoid using harsh chemicals or irritants','Drink plenty of water and maintain proper hydration']) ]
 
 @app.route('/')
 def index():
@@ -19,7 +25,7 @@ def index():
 def upload():
     image_file = request.files['image']
     if image_file:
-        img = Image.open(image_file)
+        img = Image.open(Skin-Disease)
         img = img.resize((224, 224))  # Resize image according to your model input size
         img_array = np.array(img) / 255.0  # Normalize image
         img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
